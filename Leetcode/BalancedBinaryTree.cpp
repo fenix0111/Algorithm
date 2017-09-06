7e5d142d896c421ed6a4deac49195a1134a30c74
fenix0111 <fenix0111@hotmail.com>
2017-09-06 12:08:55 +0900

    Update BalancedBinaryTree.cpp

--- Leetcode/BalancedBinaryTree.cpp
+++ Leetcode/BalancedBinaryTree.cpp
@@ -16,49 +16,49 @@
 class Solution {
 public:
     int get_depth(TreeNode* node)
-	{
-		if (node == NULL)
-		{
-			return 0;
-		}
-		else
-		{
-			int lDepth = get_depth(node->left);
-			int rDepth = get_depth(node->right);
+    {
+        if (node == NULL)
+        {
+            return 0;
+        }
+        else
+        {
+            int lDepth = get_depth(node->left);
+            int rDepth = get_depth(node->right);
 
-			if (lDepth > rDepth)
-			{
-				return(lDepth + 1);
-			}
-			else
-			{
-				return(rDepth + 1);
-			}
-		}
-	}
+            if (lDepth > rDepth)
+            {
+                return(lDepth + 1);
+            }
+            else
+            {
+                return(rDepth + 1);
+            }
+        }
+    }
 
-	bool isBalanced(TreeNode* root) 
-	{
-		if (root == NULL)
-		{
-			return true;
-		}
+    bool isBalanced(TreeNode* root) 
+    {
+        if (root == NULL)
+        {
+            return true;
+        }
 
-		if (root->left == NULL && root->right == NULL)
-		{
-			return true;
-		}
+        if (root->left == NULL && root->right == NULL)
+        {
+            return true;
+        }
 
-		int left = get_depth(root->left);
-		int right = get_depth(root->right);
+        int left = get_depth(root->left);
+        int right = get_depth(root->right);
 
-		if (abs(left - right) > 1)
-		{
-			return false;
-		}
-		else
-		{
-			return isBalanced(root->left) && isBalanced(root->right);
-		}
-	}
+        if (abs(left - right) > 1)
+        {
+            return false;
+        }
+        else
+        {
+            return isBalanced(root->left) && isBalanced(root->right);
+        }
+    }
 };
