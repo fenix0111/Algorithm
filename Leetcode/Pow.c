17568a3548da2083eca961b1f03104817368a74a
fenix0111 <fenix0111@hotmail.com>
2017-09-06 09:57:34 +0900

    Update Pow.c

--- Leetcode/Pow.c
+++ Leetcode/Pow.c
@@ -5,14 +5,14 @@
 double myPow(double x, int n) 
 {
     if (n == 0) 
-		    return 1;
+        return 1;
 
     double half = myPow(x, n / 2);
 
-	  if (n % 2 == 0) 
-		    return half * half;
-	  else if (n > 0) 
-		    return half * half * x;
-	  else 
-		    return half * half / x;
+    if (n % 2 == 0) 
+        return half * half;
+    else if (n > 0) 
+        return half * half * x;
+    else 
+        return half * half / x;
 }
