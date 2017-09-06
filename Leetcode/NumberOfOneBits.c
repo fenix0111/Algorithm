e73c0379c4cc8652dd0b2d61d9e2862fbac114e4
fenix0111 <fenix0111@hotmail.com>
2017-09-06 14:58:17 +0900

    Update NumberOfOneBits.c

--- Leetcode/NumberOfOneBits.c
+++ Leetcode/NumberOfOneBits.c
@@ -2,7 +2,8 @@
 // https://leetcode.com/problems/number-of-1-bits/description/
 // Runtime: 3ms
 
-int hammingWeight(uint32_t n) {
+int hammingWeight(uint32_t n) 
+{
     int res = 0;
     int count = 32;
     uint32_t num = n;
