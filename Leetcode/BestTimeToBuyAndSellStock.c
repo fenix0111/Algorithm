2a368283224c17ecfcb0a33e9273219c1246beaf
fenix0111 <fenix0111@hotmail.com>
2017-09-06 15:15:53 +0900

    Update BestTimeToBuyAndSellStock.c

--- Leetcode/BestTimeToBuyAndSellStock.c
+++ Leetcode/BestTimeToBuyAndSellStock.c
@@ -1,17 +1,18 @@
 // Leetcode 121. Best Time to Buy and Sell Stock
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
-// Runtime: 712 ms
+// Runtime: 712 ms (just garbage)
 
-int maxProfit(int* prices, int pricesSize) {
+int maxProfit(int* prices, int pricesSize) 
+{
     int profit = 0;
 
-	for (int i = 0; i < pricesSize - 1; i++)
-	{
-		for (int j = i + 1; j < pricesSize; j++)
-		{
-			if (prices[j] - prices[i] > 0 && (prices[j] - prices[i] > profit))
-				profit = prices[j] - prices[i];
-		}
-	}
-	return profit;
+    for (int i = 0; i < pricesSize - 1; i++)
+    {
+        for (int j = i + 1; j < pricesSize; j++)
+        {
+            if (prices[j] - prices[i] > 0 && (prices[j] - prices[i] > profit))
+                profit = prices[j] - prices[i];
+        }
+    }
+    return profit;
 }
