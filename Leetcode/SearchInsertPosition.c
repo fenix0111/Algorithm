85e39370a322f6d67560e26e617bcac20bd5ef70
fenix0111 <fenix0111@hotmail.com>
2017-09-06 15:13:17 +0900

    Update SearchInsertPosition.c

--- Leetcode/SearchInsertPosition.c
+++ Leetcode/SearchInsertPosition.c
@@ -4,42 +4,42 @@
 
 int binsearch(int *nums, int low, int high, int target)
 {
-	if (low == high)
-	{
-		if (target <= nums[low])
-		{
-			if (low == 0)
-			{
-				return 0;
-			}
-			else
-			{
-				return low;
-			}
-		}
-		else
-		{
-			return low + 1;
-		}
-	}
-	int mid = (high - low) / 2;
-
-	if (nums[low + mid] == target)
-	{
-		return low + mid;
-	}
-	else if (nums[low + mid] < target)
-	{
-		return binsearch(nums, low + mid + 1, high, target);
-	}
-	else if (nums[low + mid] > target)
-	{
-		return binsearch(nums, low, low + mid, target);
-	}
-	return;
+    if (low == high)
+    {
+        if (target <= nums[low])
+        {
+            if (low == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return low;
+            }
+        }
+        else
+        {
+            return low + 1;
+        }
+    }
+    
+    int mid = (high - low) / 2;
+    if (nums[low + mid] == target)
+    {
+        return low + mid;
+    }
+    else if (nums[low + mid] < target)
+    {
+        return binsearch(nums, low + mid + 1, high, target);
+    }
+    else if (nums[low + mid] > target)
+    {
+        return binsearch(nums, low, low + mid, target);
+    }
+    return;
 }
 
 int searchInsert(int* nums, int numsSize, int target) 
 {
-	return binsearch(nums, 0, numsSize - 1, target);
+    return binsearch(nums, 0, numsSize - 1, target);
 }
