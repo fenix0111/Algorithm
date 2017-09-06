19a2060780d909b78f6b632c8c3ec38a6001ad7f
fenix0111 <fenix0111@hotmail.com>
2017-09-06 15:26:41 +0900

    Update ConvertSortedArrayToBinarySearchTree.c

--- Leetcode/ConvertSortedArrayToBinarySearchTree.c
+++ Leetcode/ConvertSortedArrayToBinarySearchTree.c
@@ -10,19 +10,20 @@
  *     struct TreeNode *right;
  * };
  */
-struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize) 
+{
     if (numsSize == 0)
-	{
-		return NULL;
-	}
+    {
+        return NULL;
+    }
 
-	struct TreeNode *head = (struct TreeNode*)malloc(sizeof(struct TreeNode));
-	int mid = numsSize / 2;
-	int left = mid;
-	int right = numsSize - mid - 1; 
-	head->val = nums[mid];
-	head->left = sortedArrayToBST(nums, left);
-	head->right = sortedArrayToBST(nums + mid + 1, right);
-
-	return head;
+    struct TreeNode *head = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    int mid = numsSize / 2;
+    int left = mid;
+    int right = numsSize - mid - 1; 
+    head->val = nums[mid];
+    head->left = sortedArrayToBST(nums, left);
+    head->right = sortedArrayToBST(nums + mid + 1, right);
+    
+    return head;
 }
