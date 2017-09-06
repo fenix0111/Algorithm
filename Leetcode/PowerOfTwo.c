af87a1c01a1481190e9ea516ccfa3903aa636f0e
fenix0111 <fenix0111@hotmail.com>
2017-09-06 15:23:04 +0900

    Create PowerOfTwo.c

---
+++ Leetcode/PowerOfTwo.c
@@ -0,0 +1,24 @@
+// Leetcode 231. Power of Two
+// https://leetcode.com/problems/power-of-two/description/
+// Runtime: 3ms
+
+bool isPowerOfTwo(int n) {
+    if (n == 0)
+	{
+		return false;
+	}
+
+	if (n == 1)
+	{
+		return true;
+	}
+
+	if (n % 2 == 0)
+	{
+		return isPowerOfTwo(n / 2);
+	}
+	else
+	{
+		return false;
+	}
+}
