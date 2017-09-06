0f6ccca58ecb3667b7b7ed02cae75546dc8f05cc
fenix0111 <fenix0111@hotmail.com>
2017-09-06 12:56:57 +0900

    Update LengthOfLastWord.java

--- Leetcode/LengthOfLastWord.java
+++ Leetcode/LengthOfLastWord.java
@@ -5,41 +5,39 @@
 public class Solution {
     public int lengthOfLastWord(String s) {
         String[] splitted = s.split(" ");
-        
         if (splitted.length == 0) {
             return 0;
         }
         String last = splitted[splitted.length - 1];
-
         return last.length();
     }
     
     // 4ms参考解法
     public int lengthOfLastWord(String s) {
-    if (s.length() == 0) {
-      return 0;
+        if (s.length() == 0) {
+            return 0;
+        }
+        int i = s.length() - 1;
+        while (i >= 0) {
+            if (s.charAt(i) == ' ') {
+                i--;
+            } else {
+                break;
+            }
+        }
+        if (i < 0) {
+            return 0;
+        }
+        int endingIndex = i;
+        int startingIndex = i;
+        while (i >= 0) {
+            if (s.charAt(i) != ' ') {
+                startingIndex = i;
+                i--;
+            } else {
+                break;
+            }
+        }
+        return endingIndex - startingIndex + 1;
     }
-    int i = s.length() - 1;
-    while (i >= 0) {
-      if (s.charAt(i) == ' ') {
-        i--;
-      } else {
-        break;
-      }
-    }
-    if (i < 0) {
-      return 0;
-    }
-    int endingIndex = i;
-    int startingIndex = i;
-    while (i >= 0) {
-      if (s.charAt(i) != ' ') {
-        startingIndex = i;
-        i--;
-      }else {
-        break;
-      }
-    }
-    return endingIndex - startingIndex + 1;
-  }
 }
