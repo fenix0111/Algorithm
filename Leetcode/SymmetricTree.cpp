2e2a0eea602405227c5e4e097f83331f2a71e0a9
fenix0111 <fenix0111@hotmail.com>
2017-09-06 12:16:23 +0900

    Update SymmetricTree.cpp

--- Leetcode/SymmetricTree.cpp
+++ Leetcode/SymmetricTree.cpp
@@ -14,32 +14,28 @@
 class Solution {
 public:
     bool isSym(TreeNode* left, TreeNode* right)
-	{
-		if (left == NULL && right == NULL)
-		{
-			return true;
-		}
-
-		if (left && right && (left->val == right->val))
-		{
-			return isSym(left->left, right->right) && isSym(left->right, right->left);
-		}
-        
+    {
+        if (left == NULL && right == NULL)
+        {
+            return true;
+        }
+        if (left && right && (left->val == right->val))
+        {
+            return isSym(left->left, right->right) && isSym(left->right, right->left);
+        }
         return false;
-	}
+    }
 
-	bool isSymmetric(TreeNode* root) 
-	{
-		if (root == NULL)
-		{
-			return true;
-		}
-
-		if ((root->left == NULL) && (root->right == NULL))
-		{
-			return true;
-		}
-
-		return isSym(root->left, root->right);
-	}
+    bool isSymmetric(TreeNode* root) 
+    {
+        if (root == NULL)
+        {
+            return true;
+        }
+        if ((root->left == NULL) && (root->right == NULL))
+        {
+            return true;
+        }
+        return isSym(root->left, root->right);
+    }
 };
