6a88e09d95518f79d8be64500e025e516b1c3288
fenix0111 <fenix0111@hotmail.com>
2017-09-06 15:29:41 +0900

    Update ReverseInteger.c

--- Leetcode/ReverseInteger.c
+++ Leetcode/ReverseInteger.c
@@ -4,39 +4,37 @@
 
 // The input is assumed to be a 32-bit signed integer. 
 // Your function should return 0 when the reversed integer overflows.
-int reverse(int x) {
+int reverse(int x) 
+{
     long long result = 0;
-	int count = 0;
+    int count = 0;
+    int r = x;
 
-	int r = x;
+    while (r)
+    {
+        r = r / 10;
+        count++;
+    }
 
-	while (r)
-	{
-		r = r / 10;
-		count++;
-	}
+    r = x;
+    for (int i = count - 1; i >=0; i--)
+    {
+        int mod = r % 10;
+        result += mod * pow(10, i);
+        r = r / 10;
+    }
 
-	r = x;
-	for (int i = count - 1; i >=0; i--)
-	{
-		int mod = r % 10;
-		result += mod * pow(10, i);
-		r = r / 10;
-	}
-
-	if (result > INT_MAX || result < INT_MIN)
-	{
-		return 0;
-	}
-	return result;
+    if (result > INT_MAX || result < INT_MIN)
+    {
+        return 0;
+    }
+    return result;
 }
 
 // 9ms 参考解法
 int reverse(int x) 
 {
-    
     unsigned int ux, res = 0, tmp, res1;
-    
     if(x < 0)
         ux = 0 - x;
     else
@@ -54,11 +52,8 @@ int reverse(int x)
         ux = ux/10;
     }
     
-    //if((res >> 28)&0x8)
-      //  return 0;
-    
     if(x < 0)
         return 0 - (int)res;
-    return res;
     
+    return res;
 }
