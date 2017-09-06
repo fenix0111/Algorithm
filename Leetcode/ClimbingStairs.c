c609cacd34f1c58391532cc89c6b9e7f80ffce4f
fenix0111 <fenix0111@hotmail.com>
2017-09-06 15:05:23 +0900

    Update ClimbingStairs.c

--- Leetcode/ClimbingStairs.c
+++ Leetcode/ClimbingStairs.c
@@ -2,22 +2,22 @@
 // https://leetcode.com/problems/climbing-stairs/description/
 // Runtime: 0ms
 
-int climbStairs(int n) {
+int climbStairs(int n) 
+{
     if (n == 1)
-		return 1;
-	if (n == 2)
-		return 2;
+        return 1;
+    if (n == 2)
+        return 2;
 
-	int f1 = 1;
-	int f2 = 2;
-
-	int sum = 0;
-	for (int i = 3; i <= n; i++)
-	{
-		sum = f1 + f2;
-		f1 = f2;
-		f2 = sum;
-	}
-
-	return sum;
+    int f1 = 1;
+    int f2 = 2;
+    int sum = 0;
+	
+    for (int i = 3; i <= n; i++)
+    {
+        sum = f1 + f2;
+        f1 = f2;
+        f2 = sum;
+    }
+    return sum;
 }
