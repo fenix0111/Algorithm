2c1926f457bc679686aa436feb24262316415fa4
fenix0111 <fenix0111@hotmail.com>
2017-09-06 11:57:21 +0900

    Create PathSum.cpp

---
+++ Leetcode/PathSum.cpp
@@ -0,0 +1,56 @@
+// Leetcode 112. Path Sum
+// https://leetcode.com/problems/path-sum/description/
+// Runtime: 6ms
+ 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool pathSum(TreeNode* node, int sum)
+    {
+        if (node->left == NULL && node->right == NULL)
+        {
+            // 叶子节点的val刚好等于目标差
+            if (sum == node->val)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        bool lsum = false;
+        bool rsum = false;
+        if (node->left)
+            lsum = pathSum(node->left, sum - node->val);
+
+        if (node->right)
+            rsum = pathSum(node->right, sum - node->val);
+
+        return lsum || rsum;
+    }
+
+    bool hasPathSum(TreeNode* root, int sum) 
+    {
+        if (root == NULL)
+            return false;
+
+        if (root->val == sum)
+        {
+            if (root->left == NULL && root->right == NULL)
+                return true;
+            else
+                return false;
+        }
+        return pathSum(root, sum);
+    }
+};
