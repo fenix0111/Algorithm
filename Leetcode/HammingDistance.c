5663b15d91f4a23c54c16b646ce4f1c03c408901
fenix0111 <fenix0111@hotmail.com>
2017-09-06 15:46:03 +0900

    Update HammingDistance.c

--- Leetcode/HammingDistance.c
+++ Leetcode/HammingDistance.c
@@ -2,18 +2,19 @@
 // https://leetcode.com/problems/hamming-distance/description/
 // Runtime: 3ms
 
-int hammingDistance(int x, int y) {
+int hammingDistance(int x, int y) 
+{
     int vx = x;
-	int vy = y;
-	int dis = 0;
-	while (vx || vy)
-	{
-		if ((vx & 0x1) != (vy & 0x1))
-		{
-			dis++;
-		}
-		vx = vx >> 1;
-		vy = vy >> 1;
-	}
-	return dis;
+    int vy = y;
+    int dis = 0;
+    while (vx || vy)
+    {
+        if ((vx & 0x1) != (vy & 0x1))
+        {
+            dis++;
+        }
+        vx = vx >> 1;
+        vy = vy >> 1;
+    }
+    return dis;
 }
