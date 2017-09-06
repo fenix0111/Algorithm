50b8524d680e9043724904c65ff6bafa6f1ca0b7
fenix0111 <fenix0111@hotmail.com>
2017-09-06 11:21:35 +0900

    Create strStr.c

---
+++ Leetcode/strStr.c
@@ -0,0 +1,21 @@
+// Leetcode 28. Implement strStr()
+// https://leetcode.com/problems/implement-strstr/description/
+// Runtime: 3ms
+
+int strStr(char* haystack, char* needle) 
+{
+    int len_haystack = strlen(haystack);
+    int len_needle = strlen(needle);
+
+    // 被比较字符串长度大于目标字符串时直接返回-1
+    if (len_haystack < len_needle)
+        return -1;
+
+    int diff = len_haystack - len_needle;
+    for (int i = 0; i <= diff; i++)
+    {
+        if (strncmp(haystack + i, needle, len_needle) == 0)
+            return i;
+    }
+    return -1;
+}
