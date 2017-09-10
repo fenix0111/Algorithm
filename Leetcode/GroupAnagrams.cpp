03c5fed83ff9fa12c90df95965adfba9d25eac22
fenix0111 <fenix0111@hotmail.com>
2017-09-10 12:35:07 +0900

    Update GroupAnagrams.cpp

--- Leetcode/GroupAnagrams.cpp
+++ Leetcode/GroupAnagrams.cpp
@@ -1,6 +1,6 @@
 // Leetcode 49. Group Anagrams
 // https://leetcode.com/problems/group-anagrams/description/
-// Runtime: 59ms
+// Runtime: 42ms
 
 // submission里最快的是差不多26ms，鄙人的依然有提升空间
 // Leetcode 49. Group Anagrams
@@ -8,6 +8,7 @@ vector<vector<string>> groupAnagrams(vector<string>& strs)
 {
     vector< vector<string> > res;
     map<string, vector<int> > m;
+    map<string, vector<int> >::iterator it;
     string tmp;
     
     // 对于strs逐个做字符排序存到map里。
@@ -16,7 +17,8 @@ vector<vector<string>> groupAnagrams(vector<string>& strs)
     {
         tmp = strs[i];
         sort(tmp.begin(), tmp.end());
-        if (m.find(tmp) == m.end())
+        it = m.find(tmp);
+        if (it == m.end())
         {
             vector<int> v;
             v.push_back(i);
@@ -24,15 +26,15 @@ vector<vector<string>> groupAnagrams(vector<string>& strs)
         }
         else
         {
-            m.find(tmp)->second.push_back(i);
+            it->second.push_back(i);
         }
     }
     
+    vector<int> vtmp;
     for (map< string, vector<int> >::iterator it = m.begin(); it != m.end(); it++)
     {
-        vector<int> vtmp = it->second;
+        vtmp = it->second;
         vector<string> stmp;
-        
         // 从map取下标把相应下标的strs里的字符串存到结果当中
         for (int j = 0; j < vtmp.size(); j++)
         {
