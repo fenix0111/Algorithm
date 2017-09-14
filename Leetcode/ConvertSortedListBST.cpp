70b194467335d603a8a85f3e048a4287aa076abf
fenix0111 <fenix0111@hotmail.com>
2017-09-14 18:01:41 +0900

    Update ConvertSortedListBST.cpp

--- Leetcode/ConvertSortedListBST.cpp
+++ Leetcode/ConvertSortedListBST.cpp
@@ -19,58 +19,50 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// 跟数组转BST思路相同
+// 先把链表的元素放进临时数组后转为BST即可
 class Solution {
 public:
-TreeNode *makeBST(int *v, int l, int m, int r)
-{
-	if (m < l || m > r)
-		return NULL;
+    TreeNode *makeBST(int* nums, int numsSize)
+    {
+        if (numsSize == 0)
+            return NULL;
+        
+        TreeNode *head = (TreeNode*)malloc(sizeof(TreeNode));
+        int mid = numsSize / 2;
+        int left = mid;
+        int right = numsSize - mid - 1;
+        head->val = nums[mid];
+        head->left = sortedArrayToBST(nums, left);
+        head->right = sortedArrayToBST(nums + mid + 1, right);
+        return head;
+    }
 
-	int rl = m + 1;
-	int rm = rl + (r - rl) / 2;
+    TreeNode* sortedListToBST(ListNode* head) 
+    {
+        if (head == NULL)
+            return NULL;
+        
+        ListNode* pl = (ListNode*)malloc(sizeof(ListNode));
+        pl = head;
+        int size = 0;
+        while (pl)
+        {
+            size++;
+            pl = pl->next;
+        }
 
-	int lr = m - 1;
-	int lm = l + (lr - l) / 2;
-
-	TreeNode *node = (TreeNode*)malloc(sizeof(TreeNode));
-	node->val = v[m];
-	node->left = makeBST(v, l, lm, lr);
-	node->right = makeBST(v, rl, rm, r);
-	
-	return node;
-}
-
-TreeNode* sortedListToBST(ListNode* head) 
-{
-	if (head == NULL)
-	{
-		return NULL;
-	}
-
-	TreeNode* h = (TreeNode*)malloc(sizeof(TreeNode));
-	ListNode* pl = (ListNode*)malloc(sizeof(ListNode));
-	pl = head;
-
-	int size = 0;
-	while (pl)
-	{
-		size++;
-		pl = pl->next;
-	}
-
-	pl = head;
-	int *tmp = (int*)malloc(sizeof(int) * size);
-	int i = 0;
-	while (pl)
-	{
-		tmp[i] = pl->val;
-		pl = pl->next;
-		i++;
-	}
-
-	int mid = size / 2;
-	int r = size - 1;
-	h = makeBST(tmp, 0, mid, r);
-	return h;
-}
+        pl = head;
+        int *tmp = (int*)malloc(sizeof(int) * size);
+        int i = 0;
+        while (pl)
+        {
+            tmp[i] = pl->val;
+            pl = pl->next;
+            i++;
+        }
+        
+        return makeBST(tmp, size);
+    }
 };
