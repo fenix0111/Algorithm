41e98c2e69cffece17fb28b8bb0c78fdbc203ed9
fenix0111 <fenix0111@hotmail.com>
2017-09-19 00:11:15 +0900

    Update ConstructBinaryTreePreorderInorderTraversal.cpp

--- Leetcode/ConstructBinaryTreePreorderInorderTraversal.cpp
+++ Leetcode/ConstructBinaryTreePreorderInorderTraversal.cpp
@@ -11,47 +11,51 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// 不太美观。。
+// 主要思路: 先序遍历里第一个元素是根结点，根据这个根结点在中序遍历里找到根结点位置。
+// 这样在中序遍历里根结点前面部分是根结点左子树而后面部分就是右子树了。
+// 之后递归处理即可。
 class Solution {
 public:
     TreeNode* btRecur(vector<int>& preorder, vector<int>& inorder, int pb, int pe, int ib, int ie)
-{
-    if (pb > pe)
-        return NULL;
-    
-    if (ib > ie)
-        return NULL;
-    
-    int rt = 0;
-    for (int i = ib; i <= ie; i++)
     {
-        if (inorder[i] == preorder[pb])
+        if (pb > pe)
+            return NULL;
+    
+        if (ib > ie)
+            return NULL;
+    
+        int rt = 0;
+        for (int i = ib; i <= ie; i++)
         {
-            rt = i;
-            break;
+            if (inorder[i] == preorder[pb])
+            {
+                rt = i;
+                break;
+            }
         }
+    
+        int ileftl = ib;
+        int ileftr = rt - 1;
+        int irightl = rt + 1;
+        int irightr = ie;
+    
+        int pleftl = pb + 1;
+        int pleftr = pb + rt - ib;
+        int prightl = pleftr + 1;
+        int prightr = pe;
+    
+        TreeNode *tree = (TreeNode*)malloc(sizeof(TreeNode));
+        tree->val = inorder[rt];
+        tree->left = btRecur(preorder, inorder, pleftl, pleftr, ileftl, ileftr);
+        tree->right = btRecur(preorder, inorder, prightl, prightr, irightl, irightr);
+    
+        return tree;
     }
-    
-    int ileftl = ib;
-    int ileftr = rt - 1;
-    int irightl = rt + 1;
-    int irightr = ie;
-    
-    int pleftl = pb + 1;
-    int pleftr = pb + rt - ib;
-    int prightl = pleftr + 1;
-    int prightr = pe;
-    
-    TreeNode *tree = (TreeNode*)malloc(sizeof(TreeNode));
-    tree->val = inorder[rt];
-    tree->left = btRecur(preorder, inorder, pleftl, pleftr, ileftl, ileftr);
-    tree->right = btRecur(preorder, inorder, prightl, prightr, irightl, irightr);
-    
-    return tree;
 
-}
-
-TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
-{
-    return btRecur(preorder, inorder, 0, (int)preorder.size() - 1, 0, (int)inorder.size() - 1);
-}
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
+    {
+        return btRecur(preorder, inorder, 0, (int)preorder.size() - 1, 0, (int)inorder.size() - 1);
+    }
 };
