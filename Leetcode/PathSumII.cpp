be8ae468b5f93ffcbfea69eaf83abaf6ce00cd91
fenix0111 <fenix0111@hotmail.com>
2017-09-19 12:46:20 +0900

    Update PathSumII.cpp

--- Leetcode/PathSumII.cpp
+++ Leetcode/PathSumII.cpp
@@ -11,35 +11,38 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// 这个问题需要注意负数的情况
+// 而且要求是从根节点到叶子节点的和所以必须要判断当前节点是否为叶子节点
 class Solution {
 public:
-void psRecur(TreeNode *root, int sum, vector<int> sol, vector< vector<int> > &vec, int target)
-{
-	if (root == NULL)
-	{
-		if (!sol.empty())
-			sol.pop_back();
-		return;
-	}
+    void psRecur(TreeNode *root, int sum, vector<int> sol, vector< vector<int> > &vec, int target)
+    {
+        if (root == NULL)
+        {
+            if (!sol.empty())
+                sol.pop_back();
+            return;
+        }
 
-	int v = root->val;
-	sol.push_back(v);
-	if (root->left == NULL && root->right == NULL && sum + v == target)
-	{
-		vec.push_back(sol);
-		sol.pop_back();
-		return;
-	}
+        int v = root->val;
+        sol.push_back(v);
+        if (root->left == NULL && root->right == NULL && sum + v == target)
+        {
+            vec.push_back(sol);
+            sol.pop_back();
+            return;
+        }
 
-	psRecur(root->left, sum + v, sol, vec, target);
-	psRecur(root->right, sum + v, sol, vec, target);
-}
+        psRecur(root->left, sum + v, sol, vec, target);
+        psRecur(root->right, sum + v, sol, vec, target);
+    }
 
-vector<vector<int>> pathSum(TreeNode* root, int sum) 
-{
-	vector< vector<int> > res;
-	vector<int> solution;
-	psRecur(root, 0, solution, res, sum);
-	return res;
-}
+    vector<vector<int>> pathSum(TreeNode* root, int sum) 
+    {
+        vector< vector<int> > res;
+        vector<int> solution;
+        psRecur(root, 0, solution, res, sum);
+        return res;
+    }
 };
