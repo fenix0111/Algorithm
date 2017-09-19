55f95905d747dd92e6f1056f1e610167316ad4fa
fenix0111 <fenix0111@hotmail.com>
2017-09-19 12:51:13 +0900

    Update PathSumII.cpp

--- Leetcode/PathSumII.cpp
+++ Leetcode/PathSumII.cpp
@@ -16,26 +16,33 @@
 // 而且要求是从根节点到叶子节点的和所以必须要判断当前节点是否为叶子节点
 class Solution {
 public:
-    void psRecur(TreeNode *root, int sum, vector<int> sol, vector< vector<int> > &vec, int target)
+    void psRecur(TreeNode *root, int sum, vector<int> &sol, vector< vector<int> > &vec, int target)
     {
         if (root == NULL)
+            return;
+
+        int v = root->val;
+        sol.push_back(v);
+
+        if (root->left == NULL && root->right == NULL && sum + v != target)
         {
             if (!sol.empty())
                 sol.pop_back();
             return;
         }
 
-        int v = root->val;
-        sol.push_back(v);
         if (root->left == NULL && root->right == NULL && sum + v == target)
         {
-            vec.push_back(sol);
+            vector<int> v(sol);
+            vec.push_back(v);
             sol.pop_back();
             return;
         }
 
         psRecur(root->left, sum + v, sol, vec, target);
         psRecur(root->right, sum + v, sol, vec, target);
+        if (!sol.empty())
+            sol.pop_back();
     }
 
     vector<vector<int>> pathSum(TreeNode* root, int sum) 
