b4af033ffe17b826e1f38b5b7c03e3bf094c55f9
fenix0111 <fenix0111@hotmail.com>
2017-09-20 11:28:29 +0900

    Update AddingReversedNumbers.cpp

--- POJ/AddingReversedNumbers.cpp
+++ POJ/AddingReversedNumbers.cpp
@@ -10,31 +10,30 @@ using namespace std;
 
 int reverse_int(int x) 
 {
-	int rev = 0;
-	while (x != 0) 
-	{
-		rev = rev * 10 + x % 10;
-		x = x / 10;
-	}
-
-	return rev;
+    int rev = 0;
+    while (x != 0) 
+    {
+        rev = rev * 10 + x % 10;
+        x = x / 10;
+    }
+    return rev;
 }
 
 int main(int argc, char **argv)
 {
-	int cases;
-	cin >> cases;
-	while (cases)
-	{
-		int num1 = 0;
-		int num2 = 0;
-		cin >> num1 >> num2;
+    int cases;
+    cin >> cases;
+    while (cases)
+    {
+        int num1 = 0;
+        int num2 = 0;
+        cin >> num1 >> num2;
 
-		num1 = reverse_int(num1);
-		num2 = reverse_int(num2);
+        num1 = reverse_int(num1);
+        num2 = reverse_int(num2);
 
-		int sum = num1 + num2;
-		cout << reverse_int(sum) << endl;
-		cases--;
-	}
+        int sum = num1 + num2;
+        cout << reverse_int(sum) << endl;
+        cases--;
+    }
 }
