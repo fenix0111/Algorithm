0ee4efa9675a9bffeefec54939a1c10c77685543
fenix0111 <fenix0111@hotmail.com>
2017-09-21 12:28:41 +0900

    Update Search2DMatrix.c

--- Leetcode/Search2DMatrix.c
+++ Leetcode/Search2DMatrix.c
@@ -8,38 +8,39 @@ bool searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target
     if (matrixColSize == 0)
         return false;
 
-    // 这部分对第一列做搜索的可以再优化成二分查找
     int row = -1;
-    for (int i = 0; i < matrixRowSize; i++)
-    {
-        if (i == 0)
-        {
-            if (matrix[i][0] > target)
-                return false;
-            else if (matrix[i][0] == target)
-                return true;
-        }
-        else
-        {
-            if (matrix[i][0] > target)
-            {
-                row = i - 1;
-                break;
-            }
-            else if (matrix[i][0] == target)
-            {
-                return true;
-            }
-        }
-    }
-	
-    if (row == -1)
-        row = matrixRowSize - 1;
-	
     int start = 0;
-    int end = matrixColSize - 1;
+    int end = matrixRowSize - 1;
     int mid = 0;
 
+    // search in the row
+    while (start < end)
+    {
+        mid = start + (end - start) / 2;
+        int t = matrix[mid][0];
+        if (target < t)
+            end = mid - 1;
+        else if (target > t)
+            start = mid + 1;
+        else if (target == t)
+            // find it!
+            return true;
+    }
+
+    if (matrix[start][0] > target)
+        row = start - 1;
+    else if (matrix[start][0] < target)
+        row = start;
+    else
+        return true;
+
+    if (row < 0)
+        return false;
+    
+    start = 0;
+    end = matrixColSize - 1;
+    mid = 0;
+    
     // search in the row
     while (start <= end)
     {
@@ -51,7 +52,7 @@ bool searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target
             start = mid + 1;
         else if (target == t)
             // find it!
-			return true;
+            return true;
     }
     return false;
 }
