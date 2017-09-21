339fcf006fe9ad654f2b94fbf6dd271aa02af886
fenix0111 <fenix0111@hotmail.com>
2017-09-21 11:57:49 +0900

    Update Search2DMatrix.c

--- Leetcode/Search2DMatrix.c
+++ Leetcode/Search2DMatrix.c
@@ -6,62 +6,52 @@
 bool searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target) 
 {
     if (matrixColSize == 0)
-		return false;
+        return false;
 
-	int row = -1;
-	for (int i = 0; i < matrixRowSize; i++)
-	{
-		if (i == 0)
-		{
-			if (matrix[i][0] > target)
-			{
-				return false;
-			}
-			else if (matrix[i][0] == target)
-			{
-				return true;
-			}
-				
-		}
-		else
-		{
-			if (matrix[i][0] > target)
-			{
-				row = i - 1;
-				break;
-			}
-			else if (matrix[i][0] == target)
-			{
-				return true;
-			}
-		}
-	}
+    // 这部分对第一列做搜索的可以再优化成二分查找
+    int row = -1;
+    for (int i = 0; i < matrixRowSize; i++)
+    {
+        if (i == 0)
+        {
+            if (matrix[i][0] > target)
+                return false;
+            else if (matrix[i][0] == target)
+                return true;
+        }
+        else
+        {
+            if (matrix[i][0] > target)
+            {
+                row = i - 1;
+                break;
+            }
+            else if (matrix[i][0] == target)
+            {
+                return true;
+            }
+        }
+    }
 	
-	if (row == -1)
-		row = matrixRowSize - 1;
+    if (row == -1)
+        row = matrixRowSize - 1;
 	
-	int start = 0;
-	int end = matrixColSize - 1;
-	int mid = 0;
+    int start = 0;
+    int end = matrixColSize - 1;
+    int mid = 0;
 
-	// search in the row
-	while (start <= end)
-	{
-		mid = start + (end - start) / 2;
-		int t = matrix[row][mid];
-		if (target < t)
-		{
-			end = mid - 1;
-		}
-		else if (target > t)
-		{
-			start = mid + 1;
-		}
-		else if (target == t)
-		{
-			// find it!
+    // search in the row
+    while (start <= end)
+    {
+        mid = start + (end - start) / 2;
+        int t = matrix[row][mid];
+        if (target < t)
+            end = mid - 1;
+        else if (target > t)
+            start = mid + 1;
+        else if (target == t)
+            // find it!
 			return true;
-		}
-	}
-	return false;
+    }
+    return false;
 }
