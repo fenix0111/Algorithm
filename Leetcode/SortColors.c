12f7cf32841a758bc232512781dbe05cc214d00b
fenix0111 <fenix0111@hotmail.com>
2017-09-21 15:56:25 +0900

    Create SortColors.c

---
+++ Leetcode/SortColors.c
@@ -0,0 +1,45 @@
+// Leetcode 75. Sort Colors
+// https://leetcode.com/problems/sort-colors/description/
+// Runtime: 3ms
+
+// 有点奇怪的题。。用冒泡排序也能过
+void swap(int *n, int *m)
+{
+	int tmp = *n;
+	*n = *m;
+	*m = tmp;
+}
+
+void sortColors(int* nums, int numsSize) 
+{
+	int h = 0;
+	int t = numsSize - 1;
+    
+    // 先把0排到前面
+	while (h < t)
+	{
+		while (nums[h] == 0 && h < numsSize)
+			h++;
+
+		while (nums[t] != 0 && t >= 0)
+			t--;
+
+		if (h < t)
+			swap(&nums[h], &nums[t]);
+	}
+
+	t = numsSize - 1;
+
+    // 之后把1排到前面
+	while (h < t)
+	{
+		while (nums[h] == 1 && h < numsSize)
+			h++;
+
+		while (nums[t] != 1 && t >= 0)
+			t--;
+
+		if (h < t)
+			swap(&nums[h], &nums[t]);
+	}
+}
