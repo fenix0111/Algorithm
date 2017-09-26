3c1611ed9b107f2acbeda72d0e013005d63f17bd
fenix0111 <fenix0111@hotmail.com>
2017-09-26 22:10:24 +0900

    Update PartitionList.cpp

--- Leetcode/PartitionList.cpp
+++ Leetcode/PartitionList.cpp
@@ -10,74 +10,72 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// 程序有点混乱..大体思路是用三个指针撸到底
+// 因为不是链表排序所以不是很难但需要小心不少细节(比如头指针丢失问题)
 class Solution {
 public:
-    ListNode* partition(ListNode* head, int x) {
-        if (head == NULL)
-        return head;
-    
-    ListNode *front = (ListNode*)malloc(sizeof(ListNode));
-    ListNode *cur = (ListNode*)malloc(sizeof(ListNode));
-    ListNode *follow = (ListNode*)malloc(sizeof(ListNode));
-    ListNode *phead = (ListNode*)malloc(sizeof(ListNode));
-    
-    front->next = head;
-    cur = head->next;
-    follow = head;
-    phead->next = head;
-    
-    while (cur)
+    ListNode* partition(ListNode* head, int x) 
     {
-        while (front->next)
-        {
-            if (front->next->val < x)
-            {
-                front = front->next;
-            }
-            else
-            {
-                break;
-            }
-        }
-        
-        follow = front;
-        cur = front->next;
-        
+        if (head == NULL)
+            return head;
+    
+        ListNode *front = (ListNode*)malloc(sizeof(ListNode));
+        ListNode *cur = (ListNode*)malloc(sizeof(ListNode));
+        ListNode *follow = (ListNode*)malloc(sizeof(ListNode));
+    
+        front->next = head;
+        cur = head->next;
+        follow = head;
+    
         while (cur)
         {
-            if (cur->val >= x)
+            while (front->next)
             {
-                cur = cur->next;
-                follow = follow->next;
+                if (front->next->val < x)
+                    front = front->next;
+                else
+                    break;
             }
-            else
+        
+            follow = front;
+            cur = front->next;
+        
+            while (cur)
             {
-                if (cur->val < x)
+                if (cur->val >= x)
                 {
-                    if (follow == front)
+                    cur = cur->next;
+                    follow = follow->next;
+                }
+                else
+                {
+                    if (cur->val < x)
                     {
-                        follow->next = cur->next;
-                        cur->next = follow;
-                        front = cur;
-                        cur = follow->next;
-                        front = front->next;
-                    }
-                    else
-                    {
-                        follow->next = cur->next;
-                        cur->next = front->next;
-                        if (cur->next == head)
+                        if (follow == front)
                         {
-                            head = cur;
+                            follow->next = cur->next;
+                            cur->next = follow;
+                            front = cur;
+                            cur = follow->next;
+                            front = front->next;
                         }
-                        front->next = cur;
-                        front = front->next;
-                        cur = follow->next;
+                        else
+                        {
+                            follow->next = cur->next;
+                            cur->next = front->next;
+                            if (cur->next == head)
+                            {
+                                head = cur;
+                            }
+                            front->next = cur;
+                            front = front->next;
+                            cur = follow->next;
+                        }
                     }
                 }
             }
         }
-    }
-    return head;
+        return head;
     }
 };
