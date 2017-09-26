84815f8cfc8cce1c82fea821e1a02fd310f20ccb
fenix0111 <fenix0111@hotmail.com>
2017-09-26 14:43:03 +0900

    Update ValidateBST.cpp

--- Leetcode/ValidateBST.cpp
+++ Leetcode/ValidateBST.cpp
@@ -11,29 +11,27 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// 关键要定好比较范围。例如有时会出现根节点的右子树里有个左子树的右儿子节点值比根节点还要大
 class Solution {
 public:
-bool validBSTRecr(TreeNode* root, long long left, long long right)
-{
-	if (root == NULL)
-		return true;
+    bool validBSTRecr(TreeNode* root, long long left, long long right)
+    {
+        if (root == NULL)
+            return true;
 
-	if (root->val > left && root->val < right)
-	{
-		return validBSTRecr(root->left, left, root->val) && validBSTRecr(root->right, root->val, right);
-	}
-	else
-	{
-		return false;
-	}
-	
-}
+        if (root->val > left && root->val < right)
+            return validBSTRecr(root->left, left, root->val) && validBSTRecr(root->right, root->val, right);
+        else
+            return false;
+    }
 
-bool isValidBST(TreeNode* root) 
-{
-	if (root == NULL)
-		return true;
+    bool isValidBST(TreeNode* root) 
+    {
+        // 空树也算二叉搜索树?
+        if (root == NULL)
+            return true;
 
-	return validBSTRecr(root->left, LONG_MIN, root->val) && validBSTRecr(root->right, root->val, LONG_MAX);
-}
+        return validBSTRecr(root->left, LONG_MIN, root->val) && validBSTRecr(root->right, root->val, LONG_MAX);
+    }
 };
