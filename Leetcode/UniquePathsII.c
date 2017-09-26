dbe64147eda61557df36943d2af3d22407b42c50
fenix0111 <fenix0111@hotmail.com>
2017-09-26 10:02:43 +0900

    Create UniquePathsII.c

---
+++ Leetcode/UniquePathsII.c
@@ -0,0 +1,46 @@
+// Leetcode 63. Unique Paths II
+// https://leetcode.com/problems/unique-paths-ii/description/
+// Runtime: 0ms
+
+// 62题的一个变种但主要思路不变
+// 不能对有障碍物的格子进行计算
+int uniquePathsWithObstacles(int** obstacleGrid, int obstacleGridRowSize, int obstacleGridColSize) 
+{
+    int** t = (int**)malloc(sizeof(int*) * obstacleGridRowSize);
+	for (int i = 0; i < obstacleGridRowSize; i++)
+	{
+		t[i] = (int*)malloc(sizeof(int) * obstacleGridColSize);
+		memset(t[i], 0, sizeof(int) * obstacleGridColSize);
+	}
+
+	for (int i = 0; i < obstacleGridRowSize; i++)
+	{
+		if (obstacleGrid[i][0] == 1)
+		{
+			break;
+		}
+		t[i][0] = 1;
+	}
+
+	for (int i = 0; i < obstacleGridColSize; i++)
+	{
+		if (obstacleGrid[0][i] == 1)
+		{
+			break;
+		}
+		t[0][i] = 1;
+	}
+
+	for (int i = 1; i < obstacleGridRowSize; i++)
+	{
+		for (int j = 1; j < obstacleGridColSize; j++)
+		{
+			if (obstacleGrid[i][j] == 0)
+			{
+				t[i][j] = t[i - 1][j] + t[i][j - 1];
+			}
+		}
+	}
+
+	return t[obstacleGridRowSize - 1][obstacleGridColSize - 1];
+}
