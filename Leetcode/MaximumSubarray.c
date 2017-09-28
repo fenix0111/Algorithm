46c0f981e7cf029a3ca282d3559317a4d7987741
fenix0111 <fenix0111@hotmail.com>
2017-09-28 12:01:14 +0900

    Update MaximumSubarray.c

--- Leetcode/MaximumSubarray.c
+++ Leetcode/MaximumSubarray.c
@@ -1,32 +1,21 @@
 // Leetcode 53. Maximum Subarray
 // https://leetcode.com/problems/maximum-subarray/description/
-// Runtime: 252 (Grade F)
+// Runtime: 6ms
 
+// Kadane算法: https://en.wikipedia.org/wiki/Maximum_subarray_problem
 int maxSubArray(int* nums, int numsSize) 
 {
-    if (numsSize == 1)
-        return nums[0];
-    
-    int range = 0;
     int sum = INT_MIN;
-    
+    int last = 0;
     for (int i = 0; i < numsSize; i++)
     {
-        int tmp = nums[i];
-        if (tmp > sum)
-            sum = tmp;
-                
-        if (tmp < 0)
-            continue;
-			
-        for (int j = i + 1; j < numsSize; j++)
-        {
-            if (nums[j] > sum)
-                sum = nums[j];
-            tmp += nums[j];
-            if (tmp > sum)
-                sum = tmp;
-        }
+        if (nums[i] + last > sum)
+            sum = nums[i] + last;
+
+        last = last + nums[i];
+        if (last < 0)
+            last = 0;
     }
+    
     return sum;
 }
