8efcb6f0630688b4524cc38661e054379c01e106
fenix0111 <fenix0111@hotmail.com>
2017-10-03 13:46:37 +0900

    Update ReverseLinkedListII.cpp

--- Leetcode/ReverseLinkedListII.cpp
+++ Leetcode/ReverseLinkedListII.cpp
@@ -12,57 +12,57 @@
  */
  
  // 头指针处理依然是个棘手的问题
-class Solution {
+class Solution 
+{
 public:
     ListNode* reverseBetween(ListNode* head, int m, int n) 
     {
         if (head == NULL)
-        return head;
+            return head;
+   
+        if (head->next == NULL)
+            return head;
     
-    if (head->next == NULL)
-        return head;
+        if (m == n)
+            return head;
     
-    if (m == n)
-        return head;
-    
-    ListNode *pivot = (ListNode*)malloc(sizeof(ListNode));
-    ListNode *p = (ListNode*)malloc(sizeof(ListNode));
-    ListNode *q = (ListNode*)malloc(sizeof(ListNode));
-    ListNode *r = (ListNode*)malloc(sizeof(ListNode));
+        ListNode *pivot = (ListNode*)malloc(sizeof(ListNode));
+        ListNode *p = (ListNode*)malloc(sizeof(ListNode));
+        ListNode *q = (ListNode*)malloc(sizeof(ListNode));
+        ListNode *r = (ListNode*)malloc(sizeof(ListNode));
 
-    pivot->next = head;
-    int begin = 1;
-    while (begin < m)
-    {
-        pivot = pivot->next;
-        begin++;
-    }
-    
-    begin = m;
-    
-    p = pivot->next;
-    q = p->next;
-    r = q->next;
-    
-    while (begin < n)
-    {
+        pivot->next = head;
+        int begin = 1;
+        while (begin < m)
+        {
+            pivot = pivot->next;
+            begin++;
+        }
+   
+        begin = m;
+        p = pivot->next;
+        q = p->next;
         r = q->next;
-        q->next = p;
-        p = q;
-        q = r;
-        if (r)
-            r = r->next;
-        begin++;
-    }
     
-    pivot->next->next = q;
-    pivot->next = p;
+        while (begin < n)
+        {
+            r = q->next;
+            q->next = p;
+            p = q;
+            q = r;
+            if (r)
+                r = r->next;
+            begin++;
+        }
     
-    // 注意返回的指针变量
-    // 当m为1时要跟着头指针走
-    if (m == 1)
-        return p;
-    else
-        return head;
+        pivot->next->next = q;
+        pivot->next = p;
+    
+        // 注意返回的指针变量
+        // 当m为1时要跟着头指针走
+        if (m == 1)
+            return p;
+        else
+            return head;
     }
 };
