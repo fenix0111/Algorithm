d3237f6922f73c34ed157cf90d35aa69dafe922a
fenix0111 <fenix0111@hotmail.com>
2018-04-09 15:31:47 +0900

    Create SingleNumberII.cpp

---
+++ Leetcode/SingleNumberII.cpp
@@ -0,0 +1,27 @@
+// Leetcode 137. Single Number II
+// https://leetcode.com/problems/single-number-ii/description/
+// Runtime: 12ms
+
+class Solution 
+{
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        if (nums.size() == 1)
+        {
+            return nums.at(0);
+        }
+		
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 1; i = i + 3)
+        {
+            if (nums.at(i) != nums.at(i + 1))
+            {
+                return nums.at(i);
+            }
+        }
+
+        return nums.at(nums.size() - 1);
+    }
+};
