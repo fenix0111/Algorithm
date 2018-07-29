d8277331bd66d1cd9c9899b0c253b89e6e9e30a3
fenix0111 <fenix0111@hotmail.com>
2018-07-29 15:07:10 +0900

    Create LicenseKeyFormatting.java

---
+++ Leetcode/482.License Key Formatting/LicenseKeyFormatting.java
@@ -0,0 +1,41 @@
+// Leetcode 482. License Key Formatting
+// https://leetcode.com/problems/license-key-formatting/description/
+// Runtime: 17ms
+
+class Solution {
+    public String licenseKeyFormatting(String S, int K) {
+        String res = "";
+        final String DASH = "-";
+        String[] splitted = S.split(DASH);
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : splitted) {
+            sb.append(s);
+        }
+
+        String temp = sb.toString();
+        sb.setLength(0);
+
+        int len = temp.length();
+        int remaining = len % K;
+
+        int i = 0;
+
+        if (remaining > 0) {
+            sb.append(temp.substring(i, remaining));
+            i = i + remaining;
+        }
+
+        while (i < len) {
+            if (sb.length() > 0) {
+                sb.append(DASH);
+            }
+
+            sb.append(temp.substring(i, i + K));
+
+            i = i + K;
+        }
+
+        return sb.toString().toUpperCase();
+    }
+}
