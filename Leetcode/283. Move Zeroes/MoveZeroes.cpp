f5238e0465134f64864412be9b386beacb6b5153
fenix0111 <fenix0111@hotmail.com>
2018-09-10 10:06:12 +0900

    Update MoveZeroes.cpp

--- Leetcode/283. Move Zeroes/MoveZeroes.cpp
+++ Leetcode/283. Move Zeroes/MoveZeroes.cpp
@@ -7,39 +7,38 @@ class Solution
 public:
     void moveZeroes(vector<int>& nums) 
     {
-	    int len = nums.size();
+        int len = nums.size();
+        if (len == 1)
+            return;
 
-	    if (len == 1)
-		    return;
+        int pzero = 0;
+        int pnum = 0;
+        int tmp = 0;
 
-    int pzero = 0;
-    int pnum = 0;
-    int tmp = 0;
-
-    while (pnum < len && pzero < len)
-    {
-      while (pzero < len && nums[pzero] != 0)
-        pzero++;
-
-      while (pnum < len && nums[pnum] == 0)
-        pnum++;
-
-      if (pnum > pzero)
-      {
-        if (pzero < len && pnum < len)
+        while (pnum < len && pzero < len)
         {
-          tmp = nums[pzero];
-          nums[pzero] = nums[pnum];
-          nums[pnum] = tmp;
+	        while (pzero < len && nums[pzero] != 0)
+            pzero++;
 
-          pzero++;
-          pnum = pzero + 1;
+            while (pnum < len && nums[pnum] == 0)
+            pnum++;
+
+            if (pnum > pzero)
+            {
+                if (pzero < len && pnum < len)
+                {
+                    tmp = nums[pzero];
+                    nums[pzero] = nums[pnum];
+                    nums[pnum] = tmp;
+
+                    pzero++;
+                    pnum = pzero + 1;
+                }
+            }
+            else
+            {
+                pnum = pzero + 1;
+            }
         }
-      }
-      else
-      {
-        pnum = pzero + 1;
-      }
     }
-  }
 };
