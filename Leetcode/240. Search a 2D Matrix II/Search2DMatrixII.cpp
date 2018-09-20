1f7c9302e8d381d289eab54b0d25c232f2b93491
fenix0111 <fenix0111@hotmail.com>
2018-09-20 17:30:42 +0900

    Create Search2DMatrixII.cpp

---
+++ Leetcode/240. Search a 2D Matrix II/Search2DMatrixII.cpp
@@ -0,0 +1,22 @@
+// Leetcode 240. Search a 2D Matrix II
+// https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+// Runtime: 84ms
+
+// brutal force solution
+class Solution 
+{
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        vector<int> v;
+        bool isFound = false;
+        for (auto it = matrix.begin(); it != matrix.end(); it++)
+        {
+            isFound = binary_search(it->begin(), it->end(), target);
+            if (isFound)
+                return true;
+        }
+        
+        return isFound;
+    }
+};
