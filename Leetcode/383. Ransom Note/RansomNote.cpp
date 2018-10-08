bc7efa8b9458070d2f9a4cb0896a3ff5100a415b
fenix0111 <fenix0111@hotmail.com>
2018-10-08 23:35:01 +0900

    Create RansomNote.cpp

---
+++ Leetcode/383. Ransom Note/RansomNote.cpp
@@ -0,0 +1,31 @@
+// Leetcode 383. Ransom Note
+// https://leetcode.com/problems/ransom-note/description/
+// Runtime: 16ms
+
+class Solution 
+{
+public:
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+        bool ret = false;
+        unsigned short tbl[128] = {0};
+        for (int i = 0; i < magazine.size(); i++)
+        {
+            tbl[magazine[i]] += 1;
+        }
+        
+        for (int i = 0; i < ransomNote.size(); i++)
+        {
+            if (tbl[ransomNote[i]] == 0)
+            {
+                return false;
+            }
+            else
+            {
+                tbl[ransomNote[i]] -= 1;
+            }
+        }
+        
+        return true;
+    }
+};
