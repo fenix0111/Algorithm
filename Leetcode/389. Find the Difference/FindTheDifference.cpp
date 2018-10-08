99b328d5402885e8411990b2232655aac82a3614
fenix0111 <fenix0111@hotmail.com>
2018-10-09 07:20:13 +0900

    Create FindTheDifference.cpp

---
+++ Leetcode/389. Find the Difference/FindTheDifference.cpp
@@ -0,0 +1,24 @@
+// Leetcode 389. Find the Difference
+// https://leetcode.com/problems/find-the-difference/description/
+// Runtime: 0ms
+
+class Solution 
+{
+public:
+    char findTheDifference(string s, string t) 
+    {
+        char ret = 0;
+        
+        for (int i = 0; i < s.size(); i++)
+        {
+            ret = ret ^ s[i];
+        }
+        
+        for (int i = 0; i < t.size(); i++)
+        {
+            ret = ret ^ t[i];
+        }
+        
+        return ret;
+    }
+};
