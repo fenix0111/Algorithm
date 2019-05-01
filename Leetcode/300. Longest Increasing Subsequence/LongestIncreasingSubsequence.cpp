03d92137e0582a464bd4c580b765ceca948fc7cd
fenix0111 <fenix0111@hotmail.com>
2019-05-01 11:29:15 +0900

    Create LongestIncreasingSubsequence.cpp

---
+++ Leetcode/300. Longest Increasing Subsequence/LongestIncreasingSubsequence.cpp
@@ -0,0 +1,35 @@
+
+class Solution 
+{
+public:
+    int lengthOfLIS(vector<int>& nums) 
+    {
+        int sz = nums.size();
+        if (sz < 2)
+            return sz;
+        
+        int *tbl = new int[sz];
+        int max = 0;
+        for (int i = 0; i < sz; i++)
+        {
+            tbl[i] = 0;
+        }
+        
+        for (int i = 1; i < sz; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[i] > nums[j])
+                {
+                    if (tbl[i] < tbl[j] + 1)
+                        tbl[i] = tbl[j] + 1;
+                }
+                
+                if (tbl[i] > max)
+                    max = tbl[i];
+            }
+        }
+        
+        return max + 1;
+    }
+};
