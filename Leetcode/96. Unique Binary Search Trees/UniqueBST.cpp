02d164ec7247ff3b8f3f754493ab1b34875243a1
fenix0111 <fenix0111@hotmail.com>
2019-05-05 22:59:41 +0900

    Create UniqueBST.cpp

---
+++ Leetcode/96. Unique Binary Search Trees/UniqueBST.cpp
@@ -0,0 +1,29 @@
+// Leetcode 96. Unique Binary Search Trees
+// https://leetcode.com/problems/unique-binary-search-trees/
+// Runtime: 4ms
+
+class Solution {
+public:
+    int numTrees(int n) 
+    {
+        int *count = (int*)malloc(sizeof(int) * (n + 1));   
+ 
+        for (int i = 0; i <= n; i++)
+        {
+            count[i] = 0;
+        }
+        
+        count[0] = 1;
+        count[1] = 1;
+
+        for (int i = 2; i <= n; i++) 
+        {
+            for (int j = 0; j <= i - 1; j++) 
+            {
+                count[i] += count[j] * count[i - j - 1];
+            }
+        }
+
+        return count[n];
+    }
+};
