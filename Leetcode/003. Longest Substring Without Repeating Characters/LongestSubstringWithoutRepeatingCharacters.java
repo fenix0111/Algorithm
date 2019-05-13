cbabc3107e45129cc01a44bae8cf7cf3ea705ed0
fenix0111 <fenix0111@hotmail.com>
2019-05-13 21:08:09 +0900

    Update LongestSubstringWithoutRepeatingCharacters.java

--- Leetcode/003. Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.java
+++ Leetcode/003. Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,57 +1,61 @@
 // Leetcode 3. Longest Substring Without Repeating Characters
-// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
-// Runtime: 142 ms (不及格, 大部分集中于40~60ms)
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+// Runtime: 32 ms
 
-public class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int len = s.length();
-        if (len == 0 || len == 1) {
-            return len;
+class Solution 
+{
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        int sz = s.size();
+        if (sz < 1)
+        {
+            return sz;
         }
-
-        int max = 0;
-        int blen = 128;
         
-        // Lookup table, 保存相应字符是否出现过, 原始类型总是自快的。
-        // 一个boolean值只占1bit
-        boolean[] lt = new boolean[blen];
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length() - 1; i++) {
-            char ci = s.charAt(i);
-            sb.append(ci);
-            lt[ci] = true;
-
-            for (int j = i + 1; j < s.length(); j++) {
-                char cj = s.charAt(j);
-                if (lt[cj] == false) {
-                    sb.append(cj);
-                    lt[cj] = true;
-                } else {
-                    int innerLen = sb.toString().length();
-                    if (innerLen > max) {
-                        max = innerLen;
-                    }
-
-                    // 清空Stringbuilder和lookup table
-                    sb.setLength(0);
-                    for (int ii = 0; ii < blen; ii++) {
-                        lt[ii] = false;
-                    }
-                    break;
+        bool *tbl = (bool*)malloc(sizeof(bool) * 128);
+        for (int i = 0; i < 128; i++)
+        {
+            tbl[i] = false;
+        }
+        
+        int i = 0;
+        int j = 1;
+        
+        int max = 1;
+        tbl[s[0]] = true;
+        while (i < sz && j < sz)
+        {
+            if (tbl[s[j]])
+            {
+                // found same
+                for (int i = 0; i < 128; i++)
+                {
+                    tbl[i] = false;
                 }
+                
+                if (j - i > max)
+                {
+                    max = j - i;
+                }
+                
+                i++;
+                tbl[s[i]] = true;
+                j = i + 1;
             }
-
-            if (sb.toString().length() > max) {
-                max = sb.toString().length();
-            }
-
-            sb.setLength(0);
-            for (int ii = 0; ii < blen; ii++) {
-                lt[ii] = false;
+            else
+            {
+                tbl[s[j]] = true;
+                j++;
             }
         }
-
+        
+        // final check
+        if (j - i > max)
+        {
+            max = j - i;
+        }
+            
         return max;
     }
-}
+};
