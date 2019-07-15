72acfe839044f2d77022d59789094164e9eff23f
fenix0111 <fenix0111@hotmail.com>
2019-07-15 13:52:39 +0900

    Create PrintFooBarAlternately.cpp

---
+++ Leetcode_EX_3E8/1115. Print FooBar Alternately/PrintFooBarAlternately.cpp
@@ -0,0 +1,40 @@
+// Leetcode 1115. Print FooBar Alternately
+// https://leetcode.com/problems/print-foobar-alternately/
+// Runtime: 300ms
+
+class FooBar {
+private:
+    int n;
+    mutex mtx;
+    condition_variable cv;
+    bool ready = false;
+public:
+    FooBar(int n) {
+        this->n = n;
+    }
+
+    void foo(function<void()> printFoo) {  
+        for (int i = 0; i < n; i++) {
+            unique_lock<mutex> lck(mtx);
+            while (ready) 
+                cv.wait(lck);
+        	// printFoo() outputs "foo". Do not change or remove this line.
+        	printFoo();
+            ready = true;
+            cv.notify_all();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        
+        for (int i = 0; i < n; i++) {
+            unique_lock<mutex> lck(mtx);
+            while (!ready) 
+                cv.wait(lck);
+        	// printBar() outputs "bar". Do not change or remove this line.
+        	printBar();
+            ready = false;
+            cv.notify_all();
+        }
+    }
+};
