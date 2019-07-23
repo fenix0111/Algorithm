77a0c3771650e675a76a7bfac0895e7b0906e875
fenix0111 <fenix0111@hotmail.com>
2019-07-24 00:36:06 +0900

    Create ArrayPartition_I.cpp

---
+++ Leetcode/561. Array Partition I/ArrayPartition_I.cpp
@@ -0,0 +1,20 @@
+// Leetcode 561. Array Partition I
+// https://leetcode.com/problems/array-partition-i/
+// Runtime: 76ms
+
+// naive solution
+class Solution 
+{
+public:
+    int arrayPairSum(vector<int>& nums) 
+    {
+        int sum = 0;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i += 2)
+        {
+            sum += nums[i];
+        }
+        
+        return sum;
+    }
+};
