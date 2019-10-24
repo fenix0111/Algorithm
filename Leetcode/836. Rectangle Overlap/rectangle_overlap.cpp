9c72448eb613c1b103d799053027558b16e99dc5
fenix0111 <fenix0111@hotmail.com>
2019-10-24 14:26:27 +0900

    Create rectangle_overlap.cpp

---
+++ Leetcode/836. Rectangle Overlap/rectangle_overlap.cpp
@@ -0,0 +1,18 @@
+// Leetcode 836. Rectangle Overlap
+// https://leetcode.com/problems/rectangle-overlap/
+// Runtime: 0ms
+
+class Solution 
+{
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) 
+    {
+        if (rec1[2] <= rec2[0] || rec2[2] <= rec1[0])
+            return false;
+        
+        if (rec1[3] <= rec2[1] || rec2[3] <= rec1[1])
+            return false;
+        
+        return true;
+    }
+};
