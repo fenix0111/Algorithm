604823f93f264cd7b385572daf0db125016870f8
fenix0111 <fenix0111@hotmail.com>
2020-11-25 23:23:26 +0900

    Create smallest_integer_divisible_by_k.cpp

---
+++ Leetcode_EX_3E8/1015. Smallest Integer Divisible by K/smallest_integer_divisible_by_k.cpp
@@ -0,0 +1,23 @@
+// Leetcode 1015. Smallest Integer Divisible by K
+// https://leetcode.com/problems/smallest-integer-divisible-by-k/
+
+class Solution 
+{
+public:
+    int smallestRepunitDivByK(int K) 
+    {
+        if (K % 2 == 0 || K % 5 == 0) 
+            return -1;
+        
+        int remainder = 0;
+        
+        for (int len = 1; len <= K; len++) 
+        {
+            remainder = (remainder * 10 + 1) % K;
+            if (remainder == 0) {
+                return len;
+            }
+        }
+        return -1;
+    }
+};
