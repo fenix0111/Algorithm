f99580f23e0c2dc560f776ebe0bd5fe8d5829758
fenix0111 <fenix0111@hotmail.com>
2021-01-21 21:53:50 +0900

    Create find_the_most_competitive_subsequence.cpp

---
+++ Leetcode_EX_3E8/1673. Find the Most Competitive Subsequence/find_the_most_competitive_subsequence.cpp
@@ -0,0 +1,31 @@
+// Leetcode 1673. Find the Most Competitive Subsequence
+// https://leetcode.com/problems/find-the-most-competitive-subsequence/
+// time complexity: O(nk) --> k for manipulate result vector
+// space complexity: O(n)
+
+class Solution 
+{
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) 
+    {
+        vector<int> result;
+        int sz = nums.size();
+        for (int i = 0; i < sz; i++)
+        {
+            int remain = sz - i;
+            int free_space = k - result.size();
+            while (!result.empty() && remain > free_space && nums[i] < result.back())
+            {
+                result.pop_back();
+                remain--;
+            }
+
+            if (result.size() < k)
+            {
+                result.push_back(nums[i]);
+            }
+        }
+        
+        return result;
+    }
+};
