209f8870b2b64597959ad80dfffbf556943613ec
fenix0111 <fenix0111@hotmail.com>
2021-02-22 17:49:57 +0900

    Create longest_word_in_dictionary_through_deleting.cpp

---
+++ Leetcode/524. Longest Word in Dictionary through Deleting/longest_word_in_dictionary_through_deleting.cpp
@@ -0,0 +1,59 @@
+// Leetcode 524. Longest Word in Dictionary through Deleting
+// https://leetcode.com/problems/longest-word-in-dictionary-through-deleting/
+// time complexity: O(len(s) * len(d) * len(string in d))
+// space complexity: O(len(d))
+
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& d) 
+    {
+        string result = "";
+        vector<int> buf;
+        for (int k = 0; k < d.size(); k++)
+        {
+            string& word = d[k];
+            int i = 0;
+            int j = 0;
+            while (i < word.length() && j < s.length())
+            {
+                if (word[i] == s[j])
+                {
+                    i++;
+                }
+                j++;
+            }
+            
+            if (i == word.length())
+            {
+                if (buf.empty())
+                {
+                    buf.push_back(k);
+                    continue;
+                }
+                
+                if (word.length() > d[buf.back()].length())
+                {
+                    buf.clear();
+                    buf.push_back(k);
+                }
+                else if (word.length() == d[buf.back()].length())
+                {
+                    buf.push_back(k);
+                }
+            }
+        }
+        
+        if (buf.empty())
+        {
+            return result;
+        }
+        
+        sort(buf.begin(), buf.end(), [&](int x, int y) {
+            return d[x] < d[y];
+        });
+        
+        result = d[buf.front()];
+        
+        return result;
+    }
+};
