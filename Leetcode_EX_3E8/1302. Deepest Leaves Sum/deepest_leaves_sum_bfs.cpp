2501c8e6bddd465d58fefeaa59719d261a47a76b
fenix0111 <fenix0111@hotmail.com>
2021-04-11 19:04:48 +0900

    Create deepest_leaves_sum_bfs.cpp

---
+++ Leetcode_EX_3E8/1302. Deepest Leaves Sum/deepest_leaves_sum_bfs.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+    int deepestLeavesSum(TreeNode* root) 
+    {
+        queue<TreeNode*> q;
+        q.push(root);
+        int result = 0;
+        while (!q.empty())
+        {
+            int sum = 0;
+            int size = q.size();
+            while (size > 0)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                size--;
+                
+                sum += node->val;
+                
+                if (node->left)
+                {
+                    q.push(node->left);
+                }
+                
+                if (node->right)
+                {
+                    q.push(node->right);
+                }
+            }
+            
+            if (sum > 0)
+            {
+                result = sum;
+            }
+        }
+        
+        return result;
+    }
+};
