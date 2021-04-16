7147bedcede8b025f570eee75197648c0d0b2540
fenix0111 <fenix0111@hotmail.com>
2021-04-16 17:18:58 +0900

    Create remove_all_adjacent_duplicates_in_string_ii.cpp

---
+++ Leetcode_EX_3E8/1209. Remove All Adjacent Duplicates in String II/remove_all_adjacent_duplicates_in_string_ii.cpp
@@ -0,0 +1,52 @@
+// Leetcode 1209. Remove All Adjacent Duplicates in String II
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
+// time complexity: O(n)
+// space complexity: O(n)
+
+class Solution 
+{
+public:
+    string removeDuplicates(string s, int k) 
+    {
+        vector<pair<char,int>> buf; // <character, appearances>
+        int i = 0;
+        while (i < s.length())
+        {
+            if (buf.empty())
+            {
+                buf.push_back(make_pair(s[i], 1));
+            }
+            else
+            {
+                if (buf.back().first == s[i])
+                {
+                    if (buf.back().second == k - 1)
+                    {                        
+                        buf.pop_back();
+                    }
+                    else
+                    {
+                        buf.back().second++;
+                    }
+                }
+                else
+                {
+                    buf.push_back(make_pair(s[i], 1));
+                }
+            }
+            
+            i++;
+        }
+        
+        string result = "";
+        for (pair<char, int>& p : buf)
+        {
+            for (int i = 0; i < p.second; i++)
+            {
+                result.push_back(p.first);
+            }
+        }
+        
+        return result;
+    }
+};
