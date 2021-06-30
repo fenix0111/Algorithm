073685d8ef949db12e588ba3d35719f52a696d42
fenix0111 <fenix0111@hotmail.com>
2021-06-30 21:42:59 +0900

    Update lca_of_binary_tree.cpp

--- Leetcode/236. Lowest Common Ancestor of a Binary Tree/lca_of_binary_tree.cpp
+++ Leetcode/236. Lowest Common Ancestor of a Binary Tree/lca_of_binary_tree.cpp
@@ -1,6 +1,7 @@
 // Leetcode 236. Lowest Common Ancestor of a Binary Tree
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
-// Runtime: 20ms
+// time complexity: O(n)
+// space complexity: O(n)
 
 /**
  * Definition for a binary tree node.
@@ -11,73 +12,54 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- 
- // 分别找从root到p和q节点的路径
- // 逐个比较，肯定有分叉，找到这个分叉即可
- // 数组遍历完依然没有找到分叉，说明lowest common ancestor是根节点
- // 这个方法内存占用比较大
+
 class Solution 
 {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    bool findNode(vector<TreeNode*>& buf, TreeNode* node, TreeNode* target)
     {
-        if (NULL == root)
+        if (node)
         {
-            return NULL;
-        }
-        
-        vector<TreeNode*> path_p;
-        vector<TreeNode*> path_q;
-        
-        find_path(root, p, path_p);
-        find_path(root, q, path_q);
-        
-        int len = path_p.size() < path_q.size() ? path_p.size() : path_q.size();
-        int pos = 0;
-        TreeNode *ret = NULL;
-        for (int i = 0; i < len; i++)
-        {
-            if (path_p[i] != path_q[i])
+            buf.push_back(node);
+            if (node->val == target->val)
             {
-                ret = path_p[i - 1];
-                return ret;
+                return true;
+            }
+            
+            if (!findNode(buf, node->left, target) && !findNode(buf, node->right, target) && !buf.empty())
+            {
+                buf.pop_back();
+            }
+            else
+            {
+                return true;
             }
         }
         
-        ret = path_p[len - 1];
-        return ret;
+        return false;
     }
     
-    bool find_path(TreeNode *root, TreeNode *target, vector<TreeNode*> &v)
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
     {
-        if (NULL == root)
+        vector<TreeNode*> bufp;
+        vector<TreeNode*> bufq;
+        findNode(bufp, root, p);
+        findNode(bufq, root, q);
+        int i = 0;
+        TreeNode* lca = NULL;
+        while (i < bufq.size() && i < bufp.size())
         {
-            return false;
-        }
-        
-        v.push_back(root);
-        if (root->val == target->val)
-        {
-            return true;
-        }
-        
-        if (find_path(root->left, target, v))
-        {
-            return true;
-        }
-        
-        if (find_path(root->right, target, v))
-        {
-            return true;
-        }
-        else
-        {
-            if (!v.empty())
+            if (bufq[i]->val != bufp[i]->val)
             {
-                v.pop_back();
-            }    
+                break;
+            }
+            else
+            {
+                lca = bufq[i];
+                i++;
+            }
         }
         
-        return false;
+        return lca;
     }
 };
