4abe85b15869a0006cd8fd07fa14a1f87445591d
fenix0111 <fenix0111@hotmail.com>
2021-09-15 16:50:38 +0900

    Create longest_turbulent_subarray.cpp

---
+++ Leetcode/978. Longest Turbulent Subarray/longest_turbulent_subarray.cpp
@@ -0,0 +1,41 @@
+// Leetcode 978. Longest Turbulent Subarray
+// https://leetcode.com/problems/longest-turbulent-subarray/
+// time complexity: O(n)
+// space complexity: O(1)
+
+class Solution 
+{
+public:
+    int maxTurbulenceSize(vector<int>& arr) 
+    {
+        int size = arr.size();
+        if (size < 2)
+        {
+            return 1;
+        }
+        
+        int diff = arr[1] - arr[0];
+        int last_begin = 0;
+        int result = diff == 0 ? 1 : 2;
+        for (int i = 1; i < size - 1; i++)
+        {
+            int d = arr[i + 1] - arr[i];
+            if (diff == 0 || d == 0 || (diff > 0 && d > 0) || (diff < 0 && d < 0))
+            {
+                if (diff == 0)
+                {
+                    last_begin = i;
+                }
+                result = max(result, i - last_begin + 1);
+                last_begin = i;
+            }
+            
+            diff = d;
+        }
+        
+        if (diff != 0)
+            result = max(result, size - last_begin);
+        
+        return result;
+    }
+};
