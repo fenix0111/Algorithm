53a5d4208f33bac6c057fef87b6ecec9159b7acb
fenix0111 <fenix0111@hotmail.com>
2021-10-03 15:57:28 +0900

    Create jump_game_greedy.cpp

---
+++ Leetcode/055. Jump Game/jump_game_greedy.cpp
@@ -0,0 +1,20 @@
+// more easier to understand
+
+class Solution 
+{
+public:
+    bool canJump(vector<int>& nums)
+    {
+        int maxreach = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (maxreach < i)
+            {
+                return false;
+            }
+            maxreach = max(maxreach, i + nums[i]);
+        }
+
+        return true;
+    }
+};
