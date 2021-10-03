b07281e186c33a421c782d173f40009b80ef9847
fenix0111 <fenix0111@hotmail.com>
2021-10-03 15:58:59 +0900

    Update and rename jump_game_faster.cpp to jump_game_v2.cpp

--- Leetcode/055. Jump Game/jump_game_v2.cpp
+++ Leetcode/055. Jump Game/jump_game_v2.cpp
@@ -1,16 +1,16 @@
 // Leetcode 55. Jump Game
 // https://leetcode.com/problems/jump-game/
-// Runtime: 8 ms
-// Memory: 7.9 MB
+// space complexity: O(n)
+// space complexity: O(1)
 
-class Solution 
+class Solution
 {
 public:
-    bool canJump(vector<int>& nums) 
+    bool canJump(vector<int>& nums)
     {
         int end = nums.size() - 1;
         int last = nums.size() - 1; // the last point to which we can jump
-        
+
         int i = last;
         while (i > 0)
         {
@@ -21,24 +21,24 @@ public:
                 last = i + 1;
                 continue;
             }
-            
+
             // ignore 0 for now
             if (nums[i] == 0)
             {
                 i--;
                 continue;
             }
-            
+
             // in other case, just go back
             i--;
         }
-        
+
         // calculate the distance between last point and first point
         if (last - i <= nums[i] || i + nums[i] >= end)
         {
             return true;
         }
-        
+
         return false;
     }
 };
