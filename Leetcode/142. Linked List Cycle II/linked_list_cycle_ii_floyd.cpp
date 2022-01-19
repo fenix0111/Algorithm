48ba09b68b3039f4ab7745fa866b942f4be2be5c
fenix0111 <fenix0111@hotmail.com>
2022-01-19 12:56:02 +0900

    Update linked_list_cycle_ii_floyd.cpp

--- Leetcode/142. Linked List Cycle II/linked_list_cycle_ii_floyd.cpp
+++ Leetcode/142. Linked List Cycle II/linked_list_cycle_ii_floyd.cpp
@@ -6,6 +6,29 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// let the length of cycle(if it exists) be C
+// distance between head and the entry of cycle be X
+// distance between the entry of cycle and meet point of tortoise and hare be Y
+// distance from meet point to entry of cycle be Z
+
+// head------------------------entry------------meet point
+//                              |                   |
+//                              |       cycle       |
+//                              |                   |
+//                              |                   |
+//                              ---------------------
+
+// we have C = Y + Z
+// since hare has double walk speed of tortoise
+// therefore 2(X + Y) = X + C*n + Y 
+//        => X + Y = n*(Y + Z)
+//        => X = n*(Y + Z) - Y
+//        => X = (n - 1)*Y + (n - 1)*Z + Z
+//        => X = (n - 1)*C + Z
+// here, (n - 1)*C means walked the cycle several times
+// we could conclude that X = Z
+
 class Solution 
 {
 public:
