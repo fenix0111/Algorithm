7174277264984ec0be68b9582f79b3c84b4343f0
fenix0111 <fenix0111@hotmail.com>
2022-02-02 13:23:55 +0900

    Update and rename FindAllAnagramsInString.cpp to find_all_anagrams_in_string.cpp
    rename variable names

--- Leetcode/438. Find All Anagrams in a String/find_all_anagrams_in_string.cpp
+++ Leetcode/438. Find All Anagrams in a String/find_all_anagrams_in_string.cpp
@@ -1,8 +1,10 @@
 // Leetcode 438. Find All Anagrams in a String
 // https://leetcode.com/problems/find-all-anagrams-in-a-string/
-// Runtime: 28ms
+// time complexity: O(len(s) + len(p))
+// space complexity: O(len(s) + len(p))
 
-class Solution {
+class Solution
+{
 public:
     bool is_anagram(int *src, int *dst)
     {
@@ -11,10 +13,11 @@ public:
             if (src[i] != dst[i])
                 return false;
         }
+        
         return true;
     }
     
-    vector<int> findAnagrams(string s, string p) 
+    vector<int> findAnagrams(string s, string p)
     {
         int ssz = s.size();
         int psz = p.size();
@@ -23,30 +26,31 @@ public:
         if (s.empty() || ssz < psz)
             return ret;
         
-        int tbl[26] = { 0 };
-        int stb[26] = { 0 };
+        int p_tbl[26] = { 0 };
+        int s_tbl[26] = { 0 };
         
         // init source table
         for (int i = 0; i < psz; i++)
         {
-            stb[s[i] - 97] += 1;
+            s_tbl[s[i] - 97] += 1;
         }
-        
         for (int i = 0; i < psz; i++)
         {
-            tbl[p[i] - 97] += 1;
+            p_tbl[p[i] - 97] += 1;
         }
         
         for (int i = 0; i <= ssz - psz; i++)
         {
             if (i > 0)
             {
-                stb[s[i - 1] - 97] -= 1;
-                stb[s[i + psz - 1] - 97] += 1;
+                s_tbl[s[i - 1] - 97] -= 1;
+                s_tbl[s[i + psz - 1] - 97] += 1;
             }
             
-            if (is_anagram(stb, tbl))
+            if (is_anagram(s_tbl, p_tbl))
+            {
                 ret.push_back(i);
+            }
         }
         
         return ret;
