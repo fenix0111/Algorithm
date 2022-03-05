ee56105adfb3458ef5dd71bb1de14f559b38f104
fenix0111 <fenix0111@hotmail.com>
2022-03-05 15:08:01 +0900

    Update fruit_into_baskets.cpp

--- Leetcode/904. Fruit Into Baskets/fruit_into_baskets.cpp
+++ Leetcode/904. Fruit Into Baskets/fruit_into_baskets.cpp
@@ -20,6 +20,11 @@ public:
                 int remove_fruit = fruits[j];
                 
                 // move the left pointer while decreasing the fruit counts
+                // we can't just let j pointer jump to current position directly
+                // because there is a chance that suffix of current subarray would make the solution with prefix of remaining subarray
+                // ex, in the case of array "[1,2,1,1,1,3,3,3,3]"
+                // in the first stop, i would be 5 and j would be 0
+                // the solution is 7, which is the length of the subarray begin with index 2 and end with index 9
                 while (j < fruits.size() && fruits[j] == remove_fruit)
                 {
                     basket[remove_fruit]--;
