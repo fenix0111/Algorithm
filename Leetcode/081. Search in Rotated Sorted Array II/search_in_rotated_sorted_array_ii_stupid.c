66dd6d1e088a2d5e08c3f73d3f506a37a158cd2c
fenix0111 <fenix0111@hotmail.com>
2022-03-28 21:34:01 +0900

    Update and rename SearchRotatedSortedArrayII.c to search_in_rotated_sorted_array_ii_stupid.c

--- Leetcode/081. Search in Rotated Sorted Array II/search_in_rotated_sorted_array_ii_stupid.c
+++ Leetcode/081. Search in Rotated Sorted Array II/search_in_rotated_sorted_array_ii_stupid.c
@@ -2,7 +2,6 @@
 // https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
 // Runtime: 3ms
 
-// 这问题有点古怪...
 bool search(int* nums, int numsSize, int target) 
 {
     for (int i = 0; i < numsSize; i++)
