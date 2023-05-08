1386dd6147304a7db2ee1728cde634d57626a39f
Kim Jeonghwan <fenix0111@hotmail.com>
2023-05-08 23:31:23 +0900

    Create matrix_diagonal_sum.ex

---
+++ Leetcode_EX_3E8/1572. Matrix Diagonal Sum/matrix_diagonal_sum.ex
@@ -0,0 +1,21 @@
+// Leetcode 1572. Matrix Diagonal Sum
+// https://leetcode.com/problems/matrix-diagonal-sum/description/
+
+defmodule Solution do
+  @spec diagonal_sum(mat :: [[integer]]) :: integer
+  def diagonal_sum(mat) do
+    size = length(mat)
+    Enum.with_index(mat)
+    |> Enum.reduce(0, 
+      fn {row, i}, acc ->
+         left = i
+         right = size - i - 1
+
+         if left == right do
+           acc + Enum.at(row, left)
+         else
+           acc + Enum.at(row, left) + Enum.at(row, right)
+         end
+    end)
+  end
+end
