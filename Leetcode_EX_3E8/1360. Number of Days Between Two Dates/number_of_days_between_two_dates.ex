bed5ffa5e2bd7777a662c0911c08b31cc411ae42
Kim Jeonghwan <fenix0111@hotmail.com>
2023-07-08 22:44:03 +0900

    Update number_of_days_between_two_dates.ex

--- Leetcode_EX_3E8/1360. Number of Days Between Two Dates/number_of_days_between_two_dates.ex
+++ Leetcode_EX_3E8/1360. Number of Days Between Two Dates/number_of_days_between_two_dates.ex
@@ -4,10 +4,7 @@
 defmodule Solution do
   @spec days_between_dates(date1 :: String.t, date2 :: String.t) :: integer
   def days_between_dates(date1, date2) do
-    [yyyy,mm,dd] = String.split(date1, "-")
     {_, date1} =  Date.from_iso8601("#{yyyy}-#{mm}-#{dd}")
-
-    [yyyy,mm,dd] = String.split(date2, "-")
     {_, date2} =  Date.from_iso8601("#{yyyy}-#{mm}-#{dd}")
 
     abs(Date.diff(date1, date2))
